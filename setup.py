7c7c035934753208028a2d7518824f202fc59f3b
Chris Lamb <chris@chris-lamb.co.uk>
2015-10-06 23:10:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,4 +6,8 @@ setup(
     packages=(
         'django_auto_one_to_one',
     ),
+    url='https://chris-lamb.co.uk/projects/django-auto-to-one',
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    description="Automatically create and destroy child model instances",
 )
