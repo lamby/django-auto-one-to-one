6a0589db90f16c40684a9891be0a2174b1b9cc31
Peter Law <PeterJCLaw@gmail.com>
2019-09-15 15:55:39 +0100

    Fix Django 2.2 in tests
    Happily this does seem to have been a test specific issue.
    
    It's not clear why this started being an issue in 2.2 as the order
    in which the tables are created is the same. I suspect that it may
    be related to the changes to the test runner around multi-db testing
    and/or the database backend changes. In any case, forcing all apps
    to load their tables from models rather than migrations fixes this.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -10,3 +10,10 @@ DATABASES = {
         'NAME': ':memory:',
     },
 }
+
+# Tell Django to ignore migrations and create these apps' tables from the models
+# instead. This is needed in Django >= 2.2, where something changed such that if
+# the tables for our models (which don't have migrations) reference the tables
+# for contrib models (which do have migrations) then the creation of our tables
+# as part of the test setup fails.
+MIGRATION_MODULES = {x: None for x in ('auth', 'contenttypes')}
