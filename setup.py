505740ecd2c2aa811ef8f68f3a2358e7eb2a95b8
Chris Lamb <chris@chris-lamb.co.uk>
2022-06-16 09:42:46 +0100

    Releasing version 3.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-auto-one-to-one',
-    version='3.3.1',
+    version='3.3.2',
     packages=find_packages(exclude=('tests',)),
 
     url='https://chris-lamb.co.uk/projects/django-auto-one-to-one',
